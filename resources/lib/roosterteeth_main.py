25504de78f73239cd0d865864a196a76dd4c4963
skipmodea1 <skipmodea1@outlook.com>
2016-03-12 10:32:41 +0100

    v1.2.9 (2016-03-12)
    changed the listing and playing of videos
    the contextual menu item 'Play from here' is available now, it queues
    all the videos in the list from a certain point
    updated to latest requirements for a Kodi-addon
    added fanart-blurred
    added 'refresh' in contextual menu

--- resources/lib/roosterteeth_main.py
+++ resources/lib/roosterteeth_main.py
@@ -4,18 +4,12 @@
 #
 # Imports
 #
-from BeautifulSoup import BeautifulSoup
-from roosterteeth_const import __addon__, __settings__, __language__, __images_path__, __date__, __version__
-import requests
-import os
-import re
 import sys
-import urllib, urllib2
-import urlparse
-import xbmc
-import xbmcaddon
+import urllib
 import xbmcgui
 import xbmcplugin
+
+from roosterteeth_const import LANGUAGE
 
 RECENTLYADDEDURL = 'http://roosterteeth.com/episode/recently-added?page=001'
 ROOSTERTEETHSHOWSURL = 'http://www.roosterteeth.com/show/'
@@ -23,55 +17,61 @@ ACHIEVEMENTHUNTERURL = 'http://achievementhunter.com/show/'
 THEKNOWSHOWSURL = 'http://theknow.tv/show'
 FUNHAUSSHOWSURL = 'http://fun.haus/show'
 
+
 #
 # Main class
 #
 class Main:
-    def __init__( self ):
+    def __init__(self):
         #
         # Recently Added Episodes
         #
-        parameters = {"action" : "list-episodes", "plugin_category" : __language__(30000), "url" : RECENTLYADDEDURL, "next_page_possible": "True"}
+        parameters = {"action": "list-episodes", "plugin_category": LANGUAGE(30000), "url": RECENTLYADDEDURL,
+                      "next_page_possible": "True"}
         url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-        listitem = xbmcgui.ListItem( __language__(30000), iconImage="DefaultFolder.png" )
-        folder = True
-        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        list_item = xbmcgui.ListItem(LANGUAGE(30000))
+        is_folder = True
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=list_item, isFolder=is_folder)
         #
         # Roosterteeth
         #
-        parameters = {"action" : "list-shows", "plugin_category" : __language__(30001), "url" : ROOSTERTEETHSHOWSURL, "next_page_possible": "False"}
+        parameters = {"action": "list-shows", "plugin_category": LANGUAGE(30001), "url": ROOSTERTEETHSHOWSURL,
+                      "next_page_possible": "False"}
         url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-        listitem = xbmcgui.ListItem( __language__(30001), iconImage="DefaultFolder.png" )
-        folder = True
-        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        list_item = xbmcgui.ListItem(LANGUAGE(30001))
+        is_folder = True
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=list_item, isFolder=is_folder)
         #
         # Achievement Hunter
         #
-        parameters = {"action" : "list-shows", "plugin_category" : __language__(30002), "url" : ACHIEVEMENTHUNTERURL, "next_page_possible": "False"}
+        parameters = {"action": "list-shows", "plugin_category": LANGUAGE(30002), "url": ACHIEVEMENTHUNTERURL,
+                      "next_page_possible": "False"}
         url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-        listitem = xbmcgui.ListItem( __language__(30002), iconImage="DefaultFolder.png" )
-        folder = True
-        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        list_item = xbmcgui.ListItem(LANGUAGE(30002))
+        is_folder = True
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=list_item, isFolder=is_folder)
         #
         # The Know Tv
         #
-        parameters = {"action" : "list-shows", "plugin_category" : __language__(30003), "url" : THEKNOWSHOWSURL, "next_page_possible": "False"}
+        parameters = {"action": "list-shows", "plugin_category": LANGUAGE(30003), "url": THEKNOWSHOWSURL,
+                      "next_page_possible": "False"}
         url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-        listitem = xbmcgui.ListItem( __language__(30003), iconImage="DefaultFolder.png" )
-        folder = True
-        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)
+        list_item = xbmcgui.ListItem(LANGUAGE(30003))
+        is_folder = True
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=list_item, isFolder=is_folder)
 
         #
         # Fun Haus
         #
-        parameters = {"action" : "list-shows", "plugin_category" : __language__(30004), "url" : FUNHAUSSHOWSURL, "next_page_possible": "False"}
+        parameters = {"action": "list-shows", "plugin_category": LANGUAGE(30004), "url": FUNHAUSSHOWSURL,
+                      "next_page_possible": "False"}
         url = sys.argv[0] + '?' + urllib.urlencode(parameters)
-        listitem = xbmcgui.ListItem( __language__(30004), iconImage="DefaultFolder.png" )
-        folder = True
-        xbmcplugin.addDirectoryItem( handle = int(sys.argv[ 1 ] ), url = url, listitem=listitem, isFolder=folder)                        
-           
+        list_item = xbmcgui.ListItem(LANGUAGE(30004))
+        is_folder = True
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=list_item, isFolder=is_folder)
+
         # Disable sorting...
-        xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_NONE )
-        
+        xbmcplugin.addSortMethod(handle=int(sys.argv[1]), sortMethod=xbmcplugin.SORT_METHOD_NONE)
+
         # End of list...
-        xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True )
+        xbmcplugin.endOfDirectory(handle=int(sys.argv[1]), succeeded=True)
